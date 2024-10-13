4597d3e2b175bd5686a8673f63e733b36962d5a5
tdM05 <taddmao@gmail.com>
2024-10-13 12:05:20 -0400

    Created simple get api

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		// This server sends back hello world when you open the url because opening the url is a GET request
+		return "Hello, World!";
+	}
+}
